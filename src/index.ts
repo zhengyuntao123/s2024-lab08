79cafd8e176439654a6ae94ca08bd4972afb91f7
zhengyuntao123 <704344527@qq.com>
2024-03-13 13:06:35 -0400

    asyncmain

--- src/index.ts
+++ src/index.ts
@@ -54,14 +54,33 @@ function main (fileNames: string[]): void {
 // Implement the async version of the above here
 // Your version should not use .then and should use try/catch instead of .catch
 async function mainAsync(fileNames: string[]): Promise<void> {
-    // Your code here
+    fileNames.forEach(async (fileName: string) => {
+        try {
+            console.log(`Running logo detection on ${fileName}`);
+            const [result]=await client.logoDetection(fileName);
+            let scores: number[] = [];
+            const logos = result.logoAnnotations;
+            logos?.forEach((logo) => {
+                if (logo.description)
+                    console.log(`"${logo.description}" found in in file ${fileName}`);
+                if (logo.score)
+                    scores.push(logo.score);
+            });
+            const avg = scores.reduce((a, b) => a + b) / scores.length;
+            console.log(`Average score for ${fileName}: ${avg}`);
+            
+        } catch (err) {
+            if (err instanceof Error && 'code' in err && err.code === 'ENOENT')
+                console.log(`File ${fileName} not found`);
+        }
+    });
 }
 
-main([
-    './images/cmu.jpg', 
-    './images/logo-types-collection.jpg', 
-    './images/not-a-file.jpg'
-]);
+// main([
+//     './images/cmu.jpg', 
+//     './images/logo-types-collection.jpg', 
+//     './images/not-a-file.jpg'
+// ]);
 
 mainAsync([
     './images/cmu.jpg', 
